97b65d576541d27ba9280c15922ca94eb8febc25
ks563 <karen.shea90@gmail.com>
2019-01-11 19:43:06 -0600

    began js

---
+++ assets/giphy.js
@@ -0,0 +1,29 @@
+var queens = ["Alaska", "Jujubee", "Shangela", "Willam Belli", "Alyssa Edwards", "Bob the Drag Queen"];
+
+$("button").on("click", function () {
+    var searchObject = $(this).attr("data-target");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+      searchObject + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+      .then(function(response) {
+        for (var i = 0; i < results.length; i++) {
+            var gifDiv = $("<div>");
+
+            var rating = results[i].rating;
+
+            var p = $("<p>").text("Rating: " + rating);
+
+            var personImage = $("<img>");
+            personImage.attr("src", results[i].images.fixed_height.url);
+
+            gifDiv.prepend(p);
+            gifDiv.prepend(personImage);
+
+            $("#gifs-here").prepend(gifDiv);
+          }
+        });
+    });
