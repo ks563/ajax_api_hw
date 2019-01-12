8bfa28b856e2ba65f07da8a933177f502b13c8ba
ks563 <karen.shea90@gmail.com>
2019-01-12 16:18:17 -0600

    made buttons, fun fun stuff

--- assets/giphy.js
+++ assets/giphy.js
@@ -1,4 +1,4 @@
-var queens = ["Alaska", "Jujubee", "Shangela", "Willam Belli", "Alyssa Edwards", "Bob the Drag Queen"];
+var topics = ["Alaska", "Jujubee", "Shangela", "Willam Belli", "Alyssa Edwards", "Bob the Drag Queen"];
 
 $("button").on("click", function () {
     var searchObject = $(this).attr("data-target");
@@ -26,4 +26,19 @@ $("button").on("click", function () {
             $("#gifs-here").prepend(gifDiv);
           }
         });
-    });
+});
+    
+var createButtons = function () {
+  for (var j = 0; j < topics.length; j++){
+    var gifButton = $("<button>");
+    gifButton.addClass("gif-btn");
+    gifButton.attr("data-target", topic[i])
+    gifButton.text(topic[i]);
+    $("#buttonsDiv").append(gifButton);
+  }
+}
+ 
+$("#add-gif").on("click", function (event) {
+  event.preventDefault();
+  
+}
