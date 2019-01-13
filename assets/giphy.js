28fe36fa5742c0c748c26a2945356db40688c5d7
ks563 <karen.shea90@gmail.com>
2019-01-13 14:09:00 -0600

    trying to get ajax call to work with added buttons

--- assets/giphy.js
+++ assets/giphy.js
@@ -1,8 +1,7 @@
-var topics = ["Alaska Thunderfuck", "Jujubee", "Shangela", "Willam Belli", "Alyssa Edwards", "Bob the Drag Queen"];
-var ratingParam;
+var topics = ["Alaska Thunderfuck", "Jujubee", "Shangela", "Willam Belli", "Alyssa Edwards", "Bob the Drag Queen", "Manila Luzon", "Raja"];
+// var ratingParam = ["g", "pg"]
 var limit;
 
-  
 var createButtons = function () {
   // need to empty, buttons repeat when you add buttons
   $("#buttonsDiv").empty();
@@ -15,32 +14,36 @@ var createButtons = function () {
     $("#buttonsDiv").append(gifButton);
   }
 }
+
+createButtons();
  
+//new button is added but gifs are not generated when button is clicked
 $("#add-gif").on("click", function (event) {
   event.preventDefault();
   // var gif holds value of input
   var gif = $("#gif-input").val().trim();
   // pushes in to array that holds search topics - and what is looped over to create buttons
   topics.push(gif);
-  $("#gif-input").val("");
 
   // call create buttons again to make new button as items are pushed into array
   createButtons();
+  $("#gif-input").val("");
 });
 
 //on click event to start gif
 //onclick event to stop gifs
 
-createButtons();
+//as soon as I add a new button it breaks the ajax call
 
 $(".gif-btn").on("click", function () {
   var queen = $(this).attr("data-target");
   var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-    queen + "&api_key=dc6zaTOxFJmzC&limit=10";
+    queen + "&api_key=dc6zaTOxFJmzC&limit=10&rating=pg";
 
   $.ajax({
     url: queryURL,
     method: "GET",
+    rating: "g"
   })
     .then(function (response) {
       var results = response.data;
